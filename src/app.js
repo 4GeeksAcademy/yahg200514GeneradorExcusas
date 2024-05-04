2ad943859235bb2698e45546f1377fe4ad9c947e
Yahg2005 <161893815+Yahg2005@users.noreply.github.com>
2024-05-04 07:06:52 +0000

    added id

--- src/app.js
+++ src/app.js
@@ -9,25 +9,25 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 
-function generateExcuse() {
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch"
-  ];
-  let mathWho = who[Math.floor(Math.random() * who.length)];
-  let mathAction = action[Math.floor(Math.random() * action.length)];
-  let mathWhat = what[Math.floor(Math.random() * what.length)];
-  let mathWhen = when[Math.floor(Math.random() * when.length)];
-  let excuse = mathWho + " " + mathAction + " " + mathWhat + " " + mathWhen;
-  return excuse;
-}
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch"
+    ];
+    let mathWho = who[Math.floor(Math.random() * who.length)];
+    let mathAction = action[Math.floor(Math.random() * action.length)];
+    let mathWhat = what[Math.floor(Math.random() * what.length)];
+    let mathWhen = when[Math.floor(Math.random() * when.length)];
+    let excuse = mathWho + " " + mathAction + " " + mathWhat + " " + mathWhen;
+    return excuse;
+  }
 
-let excuse = generateExcuse();
+  let excuse = generateExcuse();
 
-document.querySelector("#excuse").innerText = excuse;
+  document.querySelector("#excuse").innerText = excuse;
 };
